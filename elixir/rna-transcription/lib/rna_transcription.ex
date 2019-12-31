755b899cdbebabdadc401dc94ec86265d61224df
Emir Fernandes <emir.fernandes6@gmail.com>
2019-12-30 21:14:46 -0300

    Upgrade exercise elixir/rna-transcription

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -8,25 +8,12 @@ defmodule RnaTranscription do
   'UGAC'
   """
   @spec to_rna([char]) :: [char]
-  def to_rna(dna) do
-    dna
-    |> Enum.map(fn char -> change(char) end)
+  def to_rna(?G), do: ?C
+  def to_rna(?C), do: ?G
+  def to_rna(?T), do: ?A
+  def to_rna(?A), do: ?U
+  def to_rna([]), do: []
+  def to_rna([head|tail]) do
+    [to_rna(head) | to_rna(tail)]
   end
-
-  defp change(char) do
-    case char do
-      'G' -> 'C'
-      'C' -> 'G'
-      'T' -> 'A'
-      'A' -> 'U'
-    end
-  end
-  # def to_rna('G'), do: 'C'
-  # def to_rna('C'), do: 'G'
-  # def to_rna('T'), do: 'A'
-  # def to_rna('A'), do: 'U'
-  # def to_rna([]),  do: []
-  # def to_rna([head|tail]) do
-  #   to_rna([head]) ++ to_rna(tail)
-  # end
 end
