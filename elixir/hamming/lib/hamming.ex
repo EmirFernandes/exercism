09045cdb53147e8712416046a6964bfa06369408
Emir Fernandes <emir.fernandes6@gmail.com>
2020-01-04 20:32:20 -0300

    Upgrade exercise elixir/hamming

--- elixir/hamming/lib/hamming.ex
+++ elixir/hamming/lib/hamming.ex
@@ -8,17 +8,11 @@ defmodule Hamming do
   {:ok, 4}
   """
   @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
-  def hamming_distance(s1, s2) when length(s1) != length(s2) do
-    {:error, "Lists must be the same length"}
+  def hamming_distance(s1, s2) when length(s1) == length(s2) do
+    {
+      :ok,
+      Enum.zip(s1, s2) |> Enum.count(fn {a, b} -> a != b end)
+    }
   end
-  def hamming_distance(s1, s2) do
-    {:ok, distance(s1, s2) }
-  end
-
-  defp distance([], []), do: 0
-  defp distance([h1|t1], [h2|t2]) do
-    diff = if (h1 == h2), do: 0, else: 1
-
-    diff + distance(t1, t2)
-  end
+  def hamming_distance(_, _), do: {:error, "Lists must be the same length"}
 end
